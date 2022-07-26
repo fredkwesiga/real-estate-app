90959e788218dc6e130307e753f55e6dcb8defc6
fred kwesiga <fredkwesiga9@gmail.com>
2022-07-26 21:17:53 +0300

    Setting up the project

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Flex , Box, Text, Button } from "@chakra-ui/react";
+
+const Banner = ({ purpose }) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    {purpose}
+  </Flex>
+)
+
+export default function Home() {
+  return (
+    <div>
+     <h1>hello world</h1>
+     <Banner purpose={<Text>This is a banner</Text>} />
+     <Banner  purpose={'For sale'}/>
+    </div>
+  )
+}
